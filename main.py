742a03420f068b89081aefeb67f55737d61ad6c4
sporky2311 <sporky2311@gmail.com>
2025-10-18 11:26:49 -0400

    Saving before Reinstall

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-import pygame
-def main():
-    print("Hello from asteroids-py!")
 
+#from database import connect_database, database_version
+from constants import *
+import pygame
+import os
+os.environ["SDL_VIDEODRIVER"]="x11"
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
